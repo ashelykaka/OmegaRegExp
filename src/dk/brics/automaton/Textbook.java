dbe6f930f0d2a233943d80cd0d5504c39684d7c2
huili <lihui900116@gmail.com>
2015-06-21 20:28:06 +0200

    concatenate version 1, but with just one initial state

--- src/dk/brics/automaton/Textbook.java
+++ src/dk/brics/automaton/Textbook.java
@@ -14,18 +14,18 @@ public class Textbook {
 	}
 
 	public Textbook nbaUnion(Textbook a2) {
-//		textbook union = new textbook();
-//		Set<State> initialStates = new HashSet<State>();
-//		initialStates.addAll(a1.getInitial());
-//		initialStates.addAll(a2.getInitial());
-//		union.setInitial(initialStates);
-//		Set<State> allStates = new HashSet<State>();
-//		allStates.addAll(a1.getStates());
-//		allStates.addAll(a2.getStates());
-//		union.setStates(allStates);
-//		// how about final state.
-//		return union;
-		
+		//		textbook union = new textbook();
+		//		Set<State> initialStates = new HashSet<State>();
+		//		initialStates.addAll(a1.getInitial());
+		//		initialStates.addAll(a2.getInitial());
+		//		union.setInitial(initialStates);
+		//		Set<State> allStates = new HashSet<State>();
+		//		allStates.addAll(a1.getStates());
+		//		allStates.addAll(a2.getStates());
+		//		union.setStates(allStates);
+		//		// how about final state.
+		//		return union;
+
 		return new Textbook(this.a.union(a2.a));
 	}
 
@@ -104,45 +104,80 @@ public class Textbook {
 	}
 
 	public static Textbook nbaConcat(Automaton r, Textbook s) {
-//		textbook conc = new textbook();
-//		// ...
-//		return conc;
-		return null;
+
+		Set<State> liveStates = r.getLiveStates();
+
+		HashMap<State, Transition> newTrans = new HashMap<State, Transition>();
+
+		for (State state : liveStates) {
+			// System.out.println(state);
+
+			Set<Transition> transitions = state.getTransitions();
+			for (Transition transition : transitions) {
+				// System.out.println(state + "we have transitions  "
+				// + transition.getDest());
+				if (transition.getDest().isAccept()) {
+					System.out.println("one step before final state  " + state
+							+ " with final state " + transition.getDest()
+							+ " and symbol " + transition.getMin() + " "
+							+ transition.getMax());
+					Transition t1 = new Transition(transition.getMin(),
+							transition.getMax(), s.a.getInitialState());
+					newTrans.put(state, t1);
+				}
+			}
+		}
+		for (State state : liveStates) {
+			// change final state
+			if (state.isAccept())
+				state.setAccept(false);
+		}
+		for (Entry<State, Transition> entry : newTrans.entrySet()) {
+			State from = entry.getKey();
+			Transition t = entry.getValue();
+
+			from.addTransition(t);
+		}
+			s.a.setInitialState(r.getInitialState());
+
+			State initialState = s.a.getInitialState();
+			System.out.println("initialState "+ initialState);
+
+//			System.out.println("s "+ s);
+			return s;
+			//		return null;
+		}
+
+		public static void main(String[] args) {
+			RegExp epr = new RegExp("(a|b)*b");
+			RegExp epr2 = new RegExp("a*");
+			// epr = new RegExp("a*(c|d|e)");
+			RegExp epr3 = new RegExp("(ab)*");
+
+			Automaton automaton = epr.toAutomaton();
+//			Textbook nba = new Textbook(automaton);
+			Textbook nba2 = new Textbook(epr2.toAutomaton());
+//			nba.nbaOmega();
+			nba2.nbaOmega();
+			System.out.println("NFA "+ automaton.toDot());
+//			System.out.println("(ab)* "+ epr3.toAutomaton().toDot());
+//			System.out.println("(ab)w "+ nba2.toDot());
+
+			System.out.println(Textbook.nbaConcat(automaton, nba2).toDot());
+
+			//		Textbook newnba = nba.nbaUnion(nba2);
+			//		System.out.println(nba.toDot());
+					System.out.println(nba2.toDot());
+		}
+
+		@Override
+		public String toString() {
+			return "textbook [initial=" + this.a.getInitialState() + ", states="
+					+ this.a.getStates() + "]";
+		}
+
+		public String toDot() {
+			return a.toDot();
+		}
+
 	}
-
-	public static void main(String[] args) {
-		RegExp epr = new RegExp("a*b");
-		RegExp epr2 = new RegExp("(c|d)e*");
-		// epr = new RegExp("a*(c|d|e)");
-		Automaton automaton = epr.toAutomaton();
-		Textbook nba = new Textbook(automaton);
-		Textbook nba2 = new Textbook(epr2.toAutomaton());
-
-		// System.out.println(nba);
-
-		// System.out.println(nba.isValid());
-
-		// nba.fixCondition();
-
-		// System.out.println(nba.isValid());
-
-		// System.out.println(nba);
-
-		nba.nbaOmega();
-		nba2.nbaOmega();
-
-		Textbook newnba = nba.nbaUnion(nba2);
-		System.out.println(nba.toDot());
-	}
-
-	@Override
-	public String toString() {
-		return "textbook [initial=" + this.a.getInitialState() + ", states="
-				+ this.a.getStates() + "]";
-	}
-
-	public String toDot() {
-		return a.toDot();
-	}
-
-}
